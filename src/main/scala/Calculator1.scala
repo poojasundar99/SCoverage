4fa8b7a2c379394b0f3c641daa00540903347e93
POOJA SUNDAR <poojasundar519@gmail.com>
2022-03-24 20:24:52 +0530

    Add Test cases if calculator successfully

--- src/main/scala/Calculator1.scala
+++ src/main/scala/Calculator1.scala
@@ -1,9 +1,7 @@
-class Calculator1 {
-
+class Calculator1
+{
   def add(a:Int,b:Int):Int =a+b
   def sub(a:Int,b:Int): Int = a-b
   def mul(a:Int,b:Int): Int = a*b
   def div(a:Int,b:Int): Int = a/b
-
-
 }
